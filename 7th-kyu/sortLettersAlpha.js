c79276d304c1638f30ab2974601379298c305a76
Matt Pahuta <mhpahuta@gmail.com>
2022-04-24 11:57:26 -0700

    April 24 2022 challenges

---
+++ 7th-kyu/sortLettersAlpha.js
@@ -0,0 +1,14 @@
+// *** Sort the Gift Code - 7 kyu
+/*
+  Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+*/
+function sortGiftCode(code) {
+  // ** with split,sort,join (submitted)
+  return code.split('').sort().join('')
+}
