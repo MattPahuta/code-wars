f33a1883016cf4b1c68cf2aad812e3dc3fbad3a8
Matt Pahuta <mhpahuta@gmail.com>
2022-05-03 14:47:22 -0700

    May 3 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -5,6 +5,29 @@
 // ************************************************* //
 
 
+// *** JavaScript class-like objects - 7 kyu
+/*
+For this exercise you should create a JavaScript class like object called "Animal" that takes in "name" and "type" arguments. It should have a toString method that returns a human readable string indicating the argument information passed in. It should also allow the name property to be set.
+
+The following is an example of how the final code would be used and what the expected return values should be:
+
+var dog = new Animal('Max', 'dog');
+dog.toString(); // should return 'Max is a dog'
+dog.type; // should == 'dog'
+dog.name; // should == 'Max'
+dog.name = 'Lassie'; // should set name to 'Lassie'
+*/
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  toString() {
+    return `${this.name} is a ${this.type}`
+  }
+}
+
+
 // *** Duplicate Encoder - 6 kyu
 /*
 The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
