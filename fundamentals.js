2e15474efaa31f64e1397f19469315aa38d364d0
Matt Pahuta <mhpahuta@gmail.com>
2022-06-19 14:32:30 -0700

    June 19 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,38 +4,7 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
-// Complementary DNA - 7 kyu
-/*
-In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
-Example: (input --> output)
-
-"ATTGC" --> "TAACG"
-"GTAT" --> "CATA"
-*/
-function DNAStrand(dna) {
-  // with for...of loop, conditionals, and join
-  // let compArr = [];
-  // for (let char of dna) {
-  //   if (char === 'A') {
-  //     compArr.push('T')
-  //   } else if (char === 'T') {
-  //     compArr.push('A')
-  //   } else if (char === 'C') {
-  //     compArr.push('G')
-  //   } else if (char== 'G') {
-  //     compArr.push('C')
-  //   } else {
-  //     compArr.push(char)
-  //   }
-  // }
-  // return compArr.join('')
-
-  // using a pairs object, split, map, and join
-  const pairs = {'A':'T','T':'A','C':'G','G':'C'};
-  return dna.split('').map(char => pairs[char]).join('');
-
-}
 
 // *** Sorting the Odd way! - 7 kyu
 /*
@@ -810,6 +779,38 @@ function isRubyComing(list) {
   // return rubyCount ? true : false;
 }
 
+// Complementary DNA - 7 kyu
+/*
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+*/
+function DNAStrand(dna) {
+  // with for...of loop, conditionals, and join
+  // let compArr = [];
+  // for (let char of dna) {
+  //   if (char === 'A') {
+  //     compArr.push('T')
+  //   } else if (char === 'T') {
+  //     compArr.push('A')
+  //   } else if (char === 'C') {
+  //     compArr.push('G')
+  //   } else if (char== 'G') {
+  //     compArr.push('C')
+  //   } else {
+  //     compArr.push(char)
+  //   }
+  // }
+  // return compArr.join('')
+
+  // using a pairs object, split, map, and join
+  const pairs = {'A':'T','T':'A','C':'G','G':'C'};
+  return dna.split('').map(char => pairs[char]).join('');
+
+}
 
 // *** max diff easy - 7 kyu
 /*
