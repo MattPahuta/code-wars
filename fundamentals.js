a77a4c4b98b4dae275dc203d4742da576074f1d9
Matt Pahuta <mhpahuta@gmail.com>
2022-07-17 11:27:33 -0700

    July 17 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,40 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+
+// *** Lost numbers in number sequence - 7 kyu
+/*
+An ordered sequence of numbers from 1 to N is given. One number might have been deleted from it, then the remaining numbers were mixed. Find the number that was deleted.
+
+Example:
+
+The starting array sequence is [1,2,3,4,5,6,7,8,9]
+The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+Your function should return the int 5.
+If no number was deleted from the starting array, your function should return the int 0.
+
+Note: N may be 1 or less (in the latter case, the first array will be []).
+
+Examples:
+findDeletedNumber([1,2,3,4,5], [3,4,1,5]) => 2
+findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]) => 5
+findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]) => 0
+*/
+function findDeletedNumber(arr, mixArr) {
+  // ** with filter, includes, and ternary checking for truthy/falsy (submitted)
+  // let lostNum = arr.filter(x => !mixArr.includes(x))[0]
+  // return !lostNum ? 0 : lostNum;
+
+  // using filter, indexOf and or operator:
+  return arr.filter(num => mixArr.indexOf(num) === -1)[0] || 0;
+
+  // using reduce to copare the sum of each array value:
+  // let arrSum = arr.reduce((a,b) => a + b, 0);
+  // let mixArrSum = mixArr.reduce((a,b) => a + b, 0);
+  // return arrSum - mixArrSum;
+
+}
+
 // *** Parts of a list - 7 kyu
 /*
 
