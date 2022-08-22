b218d493cf46f64c22138e273d67d0d5d572944f
Matt Haines <mhpahuta@gmail.com>
2022-08-22 16:05:47 -0700

    Add two challenges

--- index.js
+++ index.js
@@ -227,3 +227,23 @@ function sumTwoSmallestNumbers(numbers) {
 
 const makeUpperCase = str => str.toUpperCase();
 
+// *** Convert a Boolean to a String - 8 kyu
+function booleanToString(b) {
+  return b.toString();
+
+  // alternatively 
+  // return b ? 'true' : 'false';
+}
+
+// *** Rock Paper Scissors - 8 kyu 
+const rps = (p1, p2) => {
+  if (p1 === p2) return 'Draw!'; // account for same throw
+
+  if (p1 === 'rock' && p2 === 'scissors' ||
+      p1 === 'paper' && p2 === 'rock' ||
+      p1 === 'scissors' && p2 === 'paper') {
+        return 'Player 1 won!';
+      } else {
+        return 'Player 2 won!';
+      }
+}
