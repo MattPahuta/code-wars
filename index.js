4f99bc163e0e57656dd284afb28e6db8c2f8ac80
Matt Pahuta <mhpahuta@gmail.com>
2022-09-02 14:00:26 -0700

    Add Sept 2 challenges and consolidate older completed challenges

--- index.js
+++ index.js
@@ -1,5 +1,173 @@
 // *** Welcome to my Code Wars Challenges record *** //
+// ************************************************* //
 // *** All challenges completed using JavaScript *** //
+// ************************************************* //
+
+// *** Get the mean of an array - 8 kyu
+/*
+  It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+  Return the average of the given array rounded down to its nearest integer.
+
+  The array will never be empty.
+*/
+
+function getAverage(marks) {
+  let sum = 0;
+  for (let grade of marks) {
+    sum += grade;
+  }
+  return Math.floor(sum / marks.length);
+
+  // using reduce method
+  // return Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
+
+}
+
+// *** Binary Addition - 7 kyu
+/*
+  Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+  The binary number returned should be a string.
+
+  Examples:(Input1, Input2 --> Output (explanation)))
+  1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+  5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+*/
+
+function addBinary(a,b) {
+  const sum = a + b; // add the two nums, 5 + 9 => 14
+  // convert sum to binary, 14 => 1110 
+  // return binary num as a string
+  return sum.toString(2);
+
+  // more concise:
+  // return (a + b).toString(2);
+}
+
+// *** Reduce but Grow - 8 kyu
+/*
+  Given a non-empty array of integers, return the result of multiplying the values together in order. 
+  Example:
+  [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+function grow(arr) {
+  // using reduce
+  return arr.reduce((prevVal, currVal) => prevVal * currVal, 1);
+
+  // using a for loop
+  // let product = 1;
+  // for (let num of arr) {
+  //   product *= num;
+  // }
+  // return product;
+}
+
+// *** Sum of Numbers - 7 kyu
+/*
+  Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+  Note: a and b are not ordered!
+
+  Examples (a, b) --> output (explanation)
+  (1, 0) --> 1 (1 + 0 = 1)
+  (1, 2) --> 3 (1 + 2 = 3)
+  (0, 1) --> 1 (0 + 1 = 1)
+  (1, 1) --> 1 (1 since both are same)
+  (-1, 0) --> -1 (-1 + 0 = -1)
+  (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+function getSum(a, b) {
+  const numsToSum = [];
+  if (a === b) return a;
+  else if (a > b) {
+    for (let i = b; i <= a; i++) {
+      numsToSum.push(i);
+    }
+  } else {
+    for (let i = a; i <= b; i++) {
+      numsToSum.push(i);
+    }
+  }
+
+  console.log(numsToSum)
+  return numsToSum.reduce((prevVal, currVal) => prevVal + currVal, 0);
+}
+
+// *** Grasshopper - Grade book - 8 kyu
+/*
+  Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+  Numerical Score	Letter Grade
+  90 <= score <= 100	'A'
+  80 <= score < 90	'B'
+  70 <= score < 80	'C'
+  60 <= score < 70	'D'
+  0 <= score < 60	'F'
+  Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+*/
+
+function getGrade(s1, s2, s3) {
+  const avgScore = ((s1 + s2 + s3) / 3).toFixed(2);
+  if (avgScore >= 90) return 'A';
+  else if (avgScore >= 80) return 'B';
+  else if (avgScore >= 70) return 'C';
+  else if (avgScore >= 60) return 'D';
+  else return 'F';
+
+  // with a switch statement
+  // switch(true) {
+  //   case (avgScore >= 90):
+  //     return 'A';
+  //   case (avgScore >= 80):
+  //     return 'B';
+  //   case (avgScore >= 70):
+  //     return 'C';
+  //   case (avgScore >= 60):
+  //     return 'D'; 
+  //   default:
+  //     return 'F';   
+  // }
+
+}
+
+console.log(getGrade(95,90,93)) // => 'A'
+console.log(getGrade(84,79,85)) // => 'B'
+console.log(getGrade(65,70,59)) // => 'D'
+
+// *** Count by X - 8 kyu
+/*
+  Create a function with two arguments that will return an array of the first n multiples of x.
+  Assume both the given number and the number of times to count will be positive numbers greater than 0.
+  Return the results as an array or list ( depending on language ).
+
+  Examples
+  countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+  countBy(2,5) === [2,4,6,8,10]
+*/
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) { // i is your counter, counting up to and including 'n'
+    z.push(x*i) // for each iteration, push the value of x * 1
+  }
+  return z;
+}
+
+// *** Sentence Smash - 8 kyu
+/*
+  Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+  You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. 
+  Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+  Example:
+  ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+
+function smash(words) {
+  return words.join(' ').trim(); // use join to concatenate the words from the array, use trim to ensure no extra space, beginning or end
+}
 
 // *** Credit Card Mask - 7 kyu 
 /*
@@ -7,23 +175,92 @@
   Example:  ""
 */
 function maskify(cc) {
-  return cc.slice(-4).padStart(cc.length, '#');
+  // const ccArr = cc.split(''); // change string into array
+  // if (ccArr.length <= 4) return cc; // account for short cc
+  // const lastFourEls = ccArr.slice(ccArr.length -4); // get last four elements of array
+  // return ccArr.slice(0, ccArr.length - 4).map(el => { // get all but last four elements, map over array
+  //   return el = "#"; // update elements to "#"
+  // }).concat(lastFourEls).join(""); // concat with last four elements, join back to a string
+
+  // alternative solution, using padStart() string method
+  return cc.slice(-4).padStart(cc.length,'#')
 }
 
-// *** doubleChar - 8 kyu
-/*
+console.log(maskify("4556364607935616")) // => "############5616"
 
+// *** Double Char - 8 kyu
+/*
+  Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+  Examples (Input -> Output):
+  * "String"      -> "SSttrriinngg"
+  * "Hello World" -> "HHeelllloo  WWoorrlldd"
+  * "1234!_ "     -> "11223344!!__  
 */
+
+function doubleChar(str) {
+  // Initial solution
+  // let doubledStr = "";
+  // for (let char of str) {
+  //   doubledStr += char + char;
+  // }
+  // return doubledStr;
+
+  // more modern solution:
+  return str.split('').map(char => char + char).join('');
+}
+
+// console.log(doubleChar("String" ))
+// console.log(doubleChar("Hello World"))
+// console.log(doubleChar("1234!_ "))
 
 // *** Ones and Zeros - 7 kyu
-/*
-
+/* 
+ Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+ Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
 */
+
+function binaryArrayToNumber(arr) {
+  return parseInt(Number(arr.join("")), 2); // convert arr to string, wrap with Number to ensure proper type, use parseInt w/radix argument to convert to base 2
+}
 
 // *** Mumbling - 7 kyu
 /*
+  Examples:
+  accum("abcd") -> "A-Bb-Ccc-Dddd"
+  accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+  accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+  first letter of every sequence is always capitalized
+  subsequent letters always lowercase
+  display each char (index + 1) num times
 
 */
+
+function accum(s) {
+  // const result = []; // initialize an empty array
+  // const lowerStr = s.toLowerCase(); // change str to lowercase
+
+  // for (let i = 0; i < lowerStr.length; i++) { // loop through lowerStr
+  //   let str = lowerStr[i].toUpperCase(); // make new str w/each first char upperCase
+  //   for (let j = 0; j < i; j++) { // loop through each char up to index number
+  //     str += lowerStr[i]; // concat same char, lowercased, to str
+  //   }
+  //   result.push(str) // push each str as element of result array
+  // }
+
+  // return result.join("-");
+
+  // using split, map, repeat, join
+  return s.split('').map((char, index) => (char.toUpperCase() + char.toLowerCase().repeat(index))).join('-');
+
+  // same as above, but with spread
+  // return [...s].map((char, index) => (char.toUpperCase() + char.toLowerCase().repeat(index))).join("-")
+}
+
+console.log(accum("abcd"));
+console.log(accum("RqaEzty"));
+console.log(accum("cwAt"));
 
 
 // *** Uppercase a string of words - 7 kyu
