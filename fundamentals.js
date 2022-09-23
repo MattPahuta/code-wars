a086a1f4f6e286c6a7db2a8e2c066320b4e30b3c
Matt Pahuta <mhpahuta@gmail.com>
2022-09-23 13:56:40 -0700

    Add fundamental challenges, Sept 23

---
+++ fundamentals.js
@@ -0,0 +1,61 @@
+// *** Code Wars Challenges - Fundamentals Only  *** //
+// ************************************************* //
+// ************************************************* //
+// *** All challenges completed using JavaScript *** //
+// ************************************************* //
+
+// *** Flatten and sort an array - 7 kyu
+/*
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+*/
+function flattenAndSort(array) {
+  // with for loop and sort
+  // const newArr = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   newArr.push(...array[i]);
+  // }
+  // return newArr.sort((a,b) => a - b);
+
+  // with concat and sort
+  return [].concat(...array).sort((a,b) => a - b);
+
+}
+
+
+// *** Sum of Minimums! - 7 kyu 
+/*
+Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+For Example:
+[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+]
+So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+Note: You will always be given a non-empty list containing positive values.
+*/
+
+function sumOfMinimums(arr) {
+
+  // const minNums = []; // array to hold min values
+  // for (let i = 0; i < arr.length; i += 1) { // loop through arr
+  //   minNums.push(Math.min(...arr[i])) // push min val of each arr item (num array) to minNuns
+  // }
+  // return minNums.reduce((acc, cur) => acc + cur, 0); // use reduce to add all min values
+
+  // alternate for loop solution
+  // let total = 0;
+  // for (let i = 0; i < arr.length; i += 1) {
+  //   total += Math.min(...arr[i]);
+  // }
+  // return total; 
+
+  // concise reduce method 
+  return arr.reduce((pVal, cVal) => pVal + Math.min(...cVal), 0); // spreading the cVal in place
+}
+
