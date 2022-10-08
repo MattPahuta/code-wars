b252a40441ea97bced243c2e5fa3edadc2444562
Matt Pahuta <mhpahuta@gmail.com>
2022-10-07 17:36:12 -0700

    Oct 7 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,46 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+
+// *** Check the exam - 7 kyu
+/*
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string.
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+if (arr1[i] === arr2[i]) {
+  score += 4;
+}
+if (arr1[i] !== arr2[i]) {
+  scroe -= 1;
+}
+
+
+*/
+
+function checkExam(array1, array2) {
+  let score = 0; // initialize a score variable w/0 as starting value.
+  for (let i = 0; i < array1.length; i++) { // loop through array1
+    if (array1[i] === array2[i]) { // check for correct
+      score += 4;
+    } else if (array2[i] === '') { // check for blank
+      score += 0;
+    } else { // score for incorrect
+      score -= 1;
+    }    
+  }
+  return score < 0 ? 0 : score; // return 0 for neg value, return score for 0/positive
+}
+
 // *** Counting Duplicates - 6 kyu
 /*
 Count the number of Duplicates
