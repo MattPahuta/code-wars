7fd84b12cec07787a308249fc53559d7615b2306
Matt Pahuta <mhpahuta@gmail.com>
2022-10-14 19:11:41 -0700

    Oct 14 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -88,6 +88,39 @@ function twoSort(arrOfStrings) {
   // return arrOfStrings.sort()[0].split('').join('***');
 }
 
+// *** Filter out the geese - 8 kyu
+/*
+Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+The geese are any strings in the following array, which is pre-populated in your solution:
+
+  ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+For example, if this array were passed as an argument:
+
+ ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+Your function would return the following array:
+
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+*/
+function gooseFilter(birds) {
+  const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  // use filter and includes to return only birds not included in geese array
+  return birds.filter(bird => !geese.includes(bird));
+
+  // use filter with indexOf to accomplish same thing
+  // return birds.filter(bird => geese.indexOf(bird) < 0 );
+
+  // using a for loop to accomplish what filter method does in initial solution 
+  // const result =[];
+  // for (let i = 0; i < birds.length; i++) {
+  //   if (!geese.includes(birds[i])) { // if birds[i] not included in geese
+  //     result.push(birds[i]); // push birds[i] to result array
+  //   }
+  // }
+  // return result;
+}
+
 
 function monkeyCount(n) {
   const monkeyArr = []; // initialize an array to hold the monkeys!
