59f50794a09a5a3666f2beae1a3ec0607b2e21de
Matt Pahuta <mhpahuta@gmail.com>
2022-10-26 19:18:49 -0700

    Oct 26 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,35 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// *** Remove First and Last Character Part Two - 8 kyu
+/*
+You are given a string containing a sequence of character sequences separated by commas.
+
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+
+If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+*/
+function firstAndLastRemoved(str) {
+  // ** using split, pop, shift, and join plus a ternary
+  // const strArr = str.split(',')
+  // strArr.pop();
+  // strArr.shift()
+  // return strArr.length === 0 ? null : strArr.join(' ');
+
+  // ** streamlined solution with split, slice, and join with short circuiting logic
+  // * reminder: slice() returns a shallow copy of a portion of an array, here starting at position 1 and ending one before the end
+  return str.split(",").slice(1,-1).join(" ") || null;
+}
+
 // *** N-th Power - 8 kyu
 /*
 You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
