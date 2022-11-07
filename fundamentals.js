f88d458ee731ab31d55d82a1619de0a12ef3e726
Matt Pahuta <mhpahuta@gmail.com>
2022-11-07 10:44:10 -0800

    Nov 7 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,35 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// *** Multiply Word in String - 7 kyu
+/*
+You are to write a function that takes a string as its first parameter. This string will be a string of words.
+
+You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+
+Once you have the located string you are finally going to multiply by it the third provided parameter, which will also be an integer. You are additionally required to add a hyphen in between each word.
+
+Example
+modifyMultiply ("This is a string",3,5) => 'string-string-string-string-string'
+*/
+function modifyMultiply(str, loc, num) {
+  // ** Using split, for loop, push, and join
+  // const result = [];
+  // // split string into an array, slice into a single element array
+  // const strTarget= str.split(' ')[loc];
+  // // join string with ('-') 'num' of times
+  // for (let i = 0; i < num; i++) {
+  //   result.push(strTarget)
+  // }
+  // return result.join('-');
+
+  // ** use string methods split and slice with repeat
+  // return (str.split(' ')[loc] + '-').repeat(num).slice(0, -1);
+
+  // ** using Array constructor with fill, split, and join
+  return Array(num).fill(str.split(' ')[loc]).join('-');
+}
+
 
 // *** Divide and Conquer - 7 kyu
 /*
