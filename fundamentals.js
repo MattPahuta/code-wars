b5d3f1694f1a4916e2e00703d375e4b06d3beea0
Matt Pahuta <mhpahuta@gmail.com>
2022-11-11 11:53:18 -0800

    Nov 11 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,70 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// *** Simple string characters - 7 kyu
+/*
+In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters, as follows.
+
+("*'&ABCDabcde12345") => [4,5,5,3]. 
+("Codewars@codewars123.com") => [1,18,3,2]
+("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft") => [7,13,4,10])
+--the order is: uppercase letters, lowercase, numbers and special characters.
+
+function containsSpecialChars(str) {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  return specialChars.test(str);
+}
+
+console.log(containsSpecialChars('hello!')); // 👉️ true
+console.log(containsSpecialChars('abc')); // 👉️ false
+console.log(containsSpecialChars('one two')); // 👉️ false
+
+
+
+
+*/
+function simpleStrChars(str) {
+  // const strArr = str.split('');
+  // const specialChars = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`;
+  // const specCharArr = [];
+  // for (let i = 0; i < specialChars.length; i += 1) {
+  //   specCharArr.push(specialChars[i])
+  // }
+  // const numChars = ['0','1','2','3','4','5','6','7','8','9'];
+  // const lowerChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  // const upperChars = lowerChars.map(char => char.toUpperCase())
+  // const resultSpecChars = strArr.filter(char => specCharArr.includes(char))
+  // const resultNumChars = strArr.filter(char => numChars.includes(char));
+  // const resultLowerChars = strArr.filter(char => lowerChars.includes(char));
+  // const resultUpperChars = strArr.filter(char => upperChars.includes(char));
+
+  // return [resultUpperChars.length, resultLowerChars.length, resultNumChars.length, resultSpecChars.length];
+
+  // ** using regex with match 
+  let upper = str.match(/[A-Z]/g)||[]
+  let lower = str.match(/[a-z]/g)||[]
+  let num = str.match(/[0-9]/g)||[]
+  let spec = str.match(/[^A-Z0-9]/gi)||[]
+  return [upper.length, lower.length, num.length, spec.length]
+
+  // ** using a for loop, comparing each char
+  // let result = [0, 0, 0, 0];
+  // for (let i = 0; i < str.length; i += 1) {
+  //   if (str[i].toLowerCase() > str[i]) {
+  //     result[0] += 1;
+  //   } else if (str[i].toUpperCase() < str[i]) {
+  //     result[1] += 1;
+  //   } else if (str[i] >= 0 && str[i] <= 9) {
+  //     result[2] += 1;
+  //   } else {
+  //     result[3] += 1;
+  //   }
+  // }
+  // return result;
+
+}
+
+
 // *** Product of Maximums of Array - 7 kyu
 /*
 Task
