ccb2b7ec2c262968690160933d24d0774dd55076
Matt Pahuta <mhpahuta@gmail.com>
2022-11-21 15:18:12 -0800

    Nov 21 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,87 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// *** Array Leaders (Array series #3) - 7 kyu 
+/*
+Definition
+An element is leader if it is greater than The Sum all the elements to its right side.
+
+Task
+Given an array/list [] of integers , Find all the LEADERS in the array.
+
+Notes
+- Array/list size is at least 3 .
+- Array/list's numbers Will be mixture of positives , negatives and zeros
+- Repetition of numbers in the array/list could occur.
+- Returned Array/list should store the leading numbers in the same order in the original array/list .
+
+Input >> Output Examples
+arrayLeaders ([1, 2, 3, 4, 0]) ==> return [4]
+Explanation:
+4 is greater than the sum all the elements to its right side
+
+Note : The last element 0 is equal to right sum of its elements (abstract zero).
+
+arrayLeaders ([16, 17, 4, 3, 5, 2]) ==> return [17, 5, 2]
+Explanation:
+17 is greater than the sum all the elements to its right side
+
+5 is greater than the sum all the elements to its right side
+
+Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+
+arrayLeaders ([5, 2, -1]) ==> return [5, 2]
+Explanation:
+5 is greater than the sum all the elements to its right side
+
+2 is greater than the sum all the elements to its right side
+
+Note : The last element -1 is less than the sum of its right elements (abstract zero).
+
+arrayLeaders ([0, -1, -29, 3, 2]) ==> return [0, -1, 3, 2]
+Explanation:
+0 is greater than the sum all the elements to its right side
+
+-1 is greater than the sum all the elements to its right side
+
+3 is greater than the sum all the elements to its right side
+
+Note : The last element 2 is greater than the sum of its right elements (abstract zero).
+*/
+function arrayLeaders(numbers) {
+  // loop through numbers
+  // test if the current num (i) is greater than the sum of all nums to the right
+  // add num (i) to result array([]) if passes the test
+  // Using map, slice, reduce, and push w/ternary
+  // let result = [];
+  // numbers.map((num, idx) => num > numbers.slice(idx + 1).reduce((a,b) => a + b, 0) ? result.push(num) : num);
+  // return result;
+
+  // ** with a nested for loop (most clearly illustrates what's happening for each iteration)
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) { // loop through array
+    let rightSideSum = 0; // initialize sum variable for all nums to the right 
+    for (let j = i + 1; j < numbers.length; j++) { // within each outer loop, loop through nums, starting one position to the right of outer loop
+      rightSideSum += numbers[j]; // add those nums to set the current sum
+    }
+
+    if (numbers[i] > rightSideSum) { // test if current num is greater than remaining nums sum
+      result.push(numbers[i]); // add current num to result if passes test
+    }
+  }
+  // return result;
+
+  // with filter, slice, and reduce
+  // nest the slice and reduce methods within the filter method's test
+  // returns elements that pass the filter's test
+  return numbers.filter((num, idx) => numbers.slice(idx + 1).reduce((a,b) => a + b, 0) < num);
+
+}
+
+
+
+
 // *** Coding Meetup #4 - Find first Python dev - 7 kyu
 /*
 You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
