cc677463fe092500497dbd78aa51ad687082c91c
Matt Pahuta <mhpahuta@gmail.com>
2022-11-23 10:47:11 -0800

    Nov 23 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,102 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// *** Form the Minimum - 7 kyu 
+/*
+Task
+Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+Notes:
+- Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+
+Input >> Output Examples:
+minValue ([1, 3, 1])  ==> return (13)
+Explanation:
+(13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+minValue([5, 7, 5, 9, 7])  ==> return (579)
+Explanation:
+(579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications
+
+minValue([1, 9, 3, 1, 7, 4, 6, 6, 7]) return  ==> (134679)
+Explanation:
+(134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
+*/
+function minValue(values) {
+  // ** using Set to get unique nums, ...new to convert to array
+  // sorted, joined, and wrapped in Number to return a number
+  // return Number([...new Set(values)].sort((a,b) => a - b).join(''));
+ 
+  // similar, but with Array.from and parseInt
+  let uniqueArr = Array.from(new Set(values));
+  return parseInt(uniqueArr.sort().join(''));
+
+  // using filter with sort, indexOf, join, and Number
+  // const numbers = values
+  //   .filter((num, i, arr) => arr.indexOf(num) === i)
+  //   .sort()
+  //   .join('');
+
+  // return Number(numbers);
+}
+
+// *** Maximum Gap (Array series #4) - 7 kyu
+/*
+Task
+Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
+
+Notes
+- Array/list size is at least 3 .
+- Array/list's numbers Will be mixture of positives and negatives also zeros_
+- Repetition of numbers in the array/list could occur.
+- The Maximum Gap is computed Regardless the sign.
+
+Input >> Output Examples
+maxGap([13,10,5,2,9]) ==> return (4)
+Explanation:
+The Maximum Gap after sorting the array is 4 , The difference between 9 - 5 = 4 .
+
+maxGap([-3,-27,-4,-2]) ==> return (23)
+Explanation:
+The Maximum Gap after sorting the array is 23 , The difference between  |-4- (-27) | = 23 .
+Note : Regardless the sign of negativity .
+
+maxGap([-7,-42,-809,-14,-12]) ==> return (767)  
+Explanation:
+The Maximum Gap after sorting the array is 767 , The difference between  | -809- (-42) | = 767 .
+Note : Regardless the sign of negativity .
+
+maxGap([-54,37,0,64,640,0,-15]) //return (576)
+Explanation:
+The Maximum Gap after sorting the array is 576 , The difference between  | 64 - 640 | = 576 .
+Note : Regardless the sign of negativity .
+*/
+
+function maxGap(numbers) {
+
+  // ** using sort with a for loop (submitted solution)
+  const sortedNums = numbers.sort((a,b) => a - b); // sort numbers in ascending order
+  let maxGap = sortedNums[1] - sortedNums[0]; // set initial maxGap value
+
+  for (let i = 1; i < sortedNums.length; i++) { // loop through sorted nums, starting at pos 1
+    if (sortedNums[i + 1] - sortedNums[i] > maxGap) { // test for potential greater maxGap
+      maxGap = sortedNums[i + 1] - sortedNums[i]; // set new maxGap
+    }
+  }
+
+  return maxGap; // return maxGap
+
+  // ** using sort with reduce
+  // return numbers
+  // .sort((a, b) => a - b)
+  // .reduce((max, num, i) => {
+  //   let gap = numbers[i + 1] - num
+  //   if (max < gap) {
+  //     max = gap
+  //   }
+  //   return max
+  // }, 0);
+}
 
 // *** Tidy Number - 7 kyu
 /*
@@ -305,12 +401,12 @@ Notes:
 - All data will always be valid and uniform as in the example above.
 */
 
-const list1 = [
-  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
-  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
-  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
-  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
-];
+// const list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
 
 function countDevelopers(list) {
   // check for language = 'JavaScript' && continent = 'Europe'
