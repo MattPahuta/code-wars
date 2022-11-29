5604b26b22e36f3f766d4b6adbb26603f06493e3
Matt Pahuta <mhpahuta@gmail.com>
2022-11-29 10:30:02 -0800

    Nov 29 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,56 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// *** Minimum Steps - 7 kyu
+/*
+Given an array of N integers, you have to find how many times you have to add up the smallest numbers in the array until their Sum becomes greater or equal to K.
+
+Notes:
+- List size is at least 3.
+- All numbers will be positive.
+- Numbers could occur more than once , (Duplications may exist).
+- Threshold K will always be reachable.
+
+Input >> Output Examples
+minimumSteps([1, 10, 12, 9, 2, 3], 6)  ==>  return (2)
+Explanation:
+We add two smallest elements (1 + 2), their sum is 3 .
+Then we add the next smallest number to it (3 + 3) , so the sum becomes 6 .
+Now the result is greater or equal to 6 , Hence the output is (2) i.e (2) operations are required to do this .
+
+minimumSteps([8 , 9, 4, 2], 23)  ==> return (3)
+Explanation:
+We add two smallest elements (4 + 2), their sum is 6 .
+Then we add the next smallest number to it (6 + 8) , so the sum becomes 14 .
+Now we add the next smallest number (14 + 9) , so the sum becomes 23 .
+Now the result is greater or equal to 23 , Hence the output is (3) i.e (3) operations are required to do this .
+
+minimumSteps([19,98,69,28,75,45,17,98,67], 464)  ==>  return (8)
+Explanation:
+We add two smallest elements (19 + 17), their sum is 36 .
+Then we add the next smallest number to it (36 + 28) , so the sum becomes 64 .
+We need to keep doing this until the sum becomes greater or equal to K (464 in this case), which will require 8 Steps .
+
+*/
+function minimumSteps(numbers, value) {
+  // ** with sort and a for...of loop (submitted)
+  let stepCount = 0; // track steps
+  let sum = 0; // track sum
+  numbers = numbers.sort((a,b) => a - b); // sort the array
+  for (num of numbers) { // loop over numbers
+    if (num + sum >= value) { // check if current num + sum >= value
+      break;
+    }
+    sum += num; // add each num to the sum
+    stepCount += 1; // increment the stepCount
+  }
+  return stepCount;
+
+  // with sort and filter
+  // return numbers.sort((a,b) => a - b).filter((num) => (value = value - num) > 0).length;
+
+}
+
 // *** Array.diff - 6 kyu
 /*
 Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
