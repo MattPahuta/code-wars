554e9e71f0a5aa7fd988b66f1413142900eb9057
Matt Pahuta <mhpahuta@gmail.com>
2022-12-07 10:47:41 -0800

    Dec 7 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,39 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// Complementary DNA - 7 kyu
+/*
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+*/
+function DNAStrand(dna) {
+  // with for...of loop, conditionals, and join
+  // let compArr = [];
+  // for (let char of dna) {
+  //   if (char === 'A') {
+  //     compArr.push('T')
+  //   } else if (char === 'T') {
+  //     compArr.push('A')
+  //   } else if (char === 'C') {
+  //     compArr.push('G')
+  //   } else if (char== 'G') {
+  //     compArr.push('C')
+  //   } else {
+  //     compArr.push(char)
+  //   }
+  // }
+  // return compArr.join('')
+
+  // using a pairs object, split, map, and join
+  const pairs = {'A':'T','T':'A','C':'G','G':'C'};
+  return dna.split('').map(char => pairs[char]).join('');
+
+}
+
 // *** Sorting the Odd way! - 7 kyu
 /*
 Given an array of numbers, sort them in such a manner that all the odd numbers in the array are sorted in ascending order and the even numbers are sorted in descending order after the last odd number. 
