223b1da3d7b8b12694fb9bedc5c2c8cb959b121c
Matt Pahuta <mhpahuta@gmail.com>
2022-12-09 14:44:38 -0800

    Dec 9 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -6,6 +6,7 @@
 
 
 
+
 // *** Sorting the Odd way! - 7 kyu
 /*
 Given an array of numbers, sort them in such a manner that all the odd numbers in the array are sorted in ascending order and the even numbers are sorted in descending order after the last odd number. 
@@ -955,6 +956,49 @@ function partlist(arr) {
 }
 
 
+// *** Make a function that does arithmetic - 7 kyu
+/*
+Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:(Input1, Input2, Input3 --> Output)
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+*/
+
+function arithmetic(a, b, operator) {
+  // ** with a chained ternary (submitted)
+  return operator === 'add' ? a + b 
+    : operator === 'subtract' ? a - b 
+    : operator === 'multiply' ? a * b 
+    : a / b;
+
+  // with a switch statement
+  // switch(operator) {
+  //   case 'add':
+  //     return a + b;
+  //   case 'subtract':
+  //     return a - b;
+  //   case 'multiply':
+  //     return a * b;
+  //   case 'divide':
+  //     return a / b;
+  // }
+
+  // returing an object:
+  // return {
+  //   'add'     : a + b,
+  //   'subtract': a - b,
+  //   'multiply': a * b,
+  //   'divide'  : a / b
+  // }[operator]
+}
 
 
 // *** Substring fun - 7 kyu
