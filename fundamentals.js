7959ac66f6c9b05872c80bde99c7642f40cdb08b
Matt Pahuta <mhpahuta@gmail.com>
2022-12-17 10:56:23 -0800

    Dec 17 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,22 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+// *** Sort the Gift Code - 7 kyu
+/*
+  Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+Examples (Input -- => Output):
+"abcdef"                      -- => "abcdef"
+"pqksuvy"                     -- => "kpqsuvy"
+"zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+*/
+function sortGiftCode(code) {
+  // ** with split,sort,join (submitted)
+  return code.split('').sort().join('')
+}
+
+
 
 // *** Bumps in the Road - 7 kyu
 /*
