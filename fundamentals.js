2a354eb4c9fc85de78e6995cd953935dcc5e3eb1
Matt Pahuta <mhpahuta@gmail.com>
2022-12-20 12:19:57 -0800

    Dec 20 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -5,6 +5,25 @@
 // ************************************************* //
 
 
+// *** Switcheroo - 7 kyu
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+function switcheroo(str) {
+  // accept string consisting of 'a', 'b', 'c'
+  // switch positions of 'a' and 'b', leave instances of 'c' alone
+  // ** using split, map, and join with a ternary (submitted)
+  return str.split('').map(letter => {
+    return letter === 'a' ? 'b' : letter === 'b' ? 'a' : 'c'
+  }).join('')
+}
+
+
 // *** Sum consecutives - 6 kyu
 /*
 You are given a list/array which contains only integers (positive and negative). Your job is to sum only the numbers that are the same and consecutive. The result should be one list.
