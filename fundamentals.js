f596af68f683f770d630c8175563bd213d575ca3
Matt Pahuta <mhpahuta@gmail.com>
2022-12-21 11:52:51 -0800

    Dec 21 2022 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -5,6 +5,25 @@
 // ************************************************* //
 
 
+// *** Currying functions: multiply all elements in an array - 7 kyu
+/*
+To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+Example:
+
+multiplyAll([1, 2, 3]) (2) = [2, 4, 6];
+You must not mutate the original array.
+*/
+
+function multiplyAll(arr) {
+  // take in an array of integers 
+  return (int) => arr.map(num => num * int);
+}
+
+
+
 // *** Switcheroo - 7 kyu
 /*
 Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
