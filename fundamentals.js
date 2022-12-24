af3b52e7a2f46367de2e970b5f91c56cc59d5f83
Matt Pahuta <mhpahuta@gmail.com>
2022-12-24 11:35:29 -0800

    Dec 24 2022 challenge

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,80 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+
+// *** Take a Ten Minute Walk - 6 kyu
+/*
+Receive an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']);
+
+You always walk a single block for each letter (direction)
+It takes you one minute to traverse one city block
+You must return to the starting point
+
+isValidWalk(['n','s','n','s','n','s','n','s','n','s']) 'should return true');
+
+isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']) 'should return false');
+
+isValidWalk(['w']) 'should return false');
+
+isValidWalk(['n','n','n','s','n','s','n','s','n','s']) 'should return false');
+
+
+*/
+function isValidWalk(walk) {
+  // with for...of loop, conditionals, and ternary (submitted)
+  // if (walk.length !== 10) return false;
+  // let northSouth = 0;
+  // let eastWest = 0;
+  // // for every single block we walk, increment and decrement by 1
+  // for (let dir of walk) {
+  //   if (dir === 'n') northSouth += 1;
+  //   if (dir === 's') northSouth -= 1; 
+  //   if (dir === 'e') eastWest += 1;
+  //   if (dir === 'w') eastWest -= 1;
+  // }
+  // return northSouth === 0 && eastWest === 0 ? true : false; 
+
+  // with filter and ternary
+  const north = walk.filter(dir => dir === 'n').length
+  const south = walk.filter(dir => dir === 's').length
+  const east = walk.filter(dir => dir === 'e').length
+  const west = walk.filter(dir => dir === 'w').length
+
+  return walk.length === 10 && north === south && east === west;
+
+}
+
+
+// *** Is every value in the array an array? - 7 kyu
+/*
+Is every value in the array an array?
+
+This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+Examples:
+
+[[1],[2]] => true
+['1','2'] => false
+[{1:1},{2:2}] => false
+
+*/
+function arrCheck(arr) {
+
+  // with for...of loop and isArray method
+  // for (let item of arr) {
+  //   if (!Array.isArray(item)){
+  //     return false;
+  //   }
+  // }
+  // return true;
+
+  // simplified with every method
+  return arr.every(Array.isArray)
+
+}
+
+
+
 // *** Halving Sum - 7 kyu
 /*
 Given a positive integer n, calculate the following sum:
