1b246a41fc907c9ddda514e48a8ad8f2aded48bd
Matt Pahuta <mhpahuta@gmail.com>
2023-01-03 13:52:12 -0800

    Jan 3 2023 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -5,6 +5,7 @@
 // ************************************************* //
 
 
+
 // *** Last Survivor  - 7 kyu
 /*
 You are given a string of letters and an array of numbers.
@@ -169,9 +170,27 @@ function findMissing(arr1, arr2) {
 
   // with reduce - comparing the difference in sum between the two arrays
   return arr1.reduce((pre, val) => pre + val) - arr2.reduce((pre, val) => pre + val, 0);
-
   
 }
+
+// *** Largest Elements - 7 kyu
+/*
+Write a program that outputs the top n elements from a list.
+- if n === 0, return an empty []
+Example:
+largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+*/
+function largest(n, arr) {
+  // with ternary, sort, and slice (submitted)
+  // if n > 0, sort array and return array with last n elements
+  // if n <= 0, return []
+  return n ? arr.sort((a,b) => a - b).slice(-n) : [];
+
+  // with sort and slice (slice starting val equal or larger than arr length results in [])
+  // return arr.sort((a,b) => a - b).slice(arr.length -n);
+}
+
 
 
 
