93de95b94f659693f03cf6c4bfa3bf1573ccbaca
Matt Pahuta <mhpahuta@gmail.com>
2023-01-09 09:10:23 -0800

    Jan 9 2023 challenges

--- index.js
+++ index.js
@@ -171,25 +171,6 @@ function stantonMeasure(arr) {
 }
 
 
-// *** Number of People in the Bus - 7 kyu
-/*
-  Accept a two-dimensional array (busStops), i.e. [ [10, 0], [4, 2], [7, 5] ]
-  first el of nested array = passengers getting on
-  second el of nested array = people getting off bus (at bus stop)
-  return number of people remaining on bus
-*/
-const numberOfPassenger = (busStops) => { 
-  let totalPassengers = 0;
-  for (let i = 0; i < busStops.length; i++) {
-    totalPassengers += busStops[i][0];
-    totalPassengers -= busStops[i][1];
-  }
-  return totalPassengers;
-
-  // using reduce
-  // return busStops.reduce((rem, [on, off]) => rem + on - off, 0);
-}
-
 
 // *** Century from Year - 8 kyu
 /*
