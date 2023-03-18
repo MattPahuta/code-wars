1aa2ee4042d20e03192042b11e14cf164997fb87
Matt Pahuta <mhpahuta@gmail.com>
2023-03-18 13:53:01 -0700

    March 18 2023 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -28,16 +28,7 @@ Notes:
 
 The input array and continent names will always be valid and formatted as in the list above for example 'Africa' will always start with upper-case 'A'.
 */
-const devsList = [
-  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
-  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
-  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
-  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
-  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
-];
-
 function allContinents(list) {
-
   // with new Set, map, and size
   return new Set(list.map(dev => dev.continent)).size === 5; // check there are 5 unique elements 
 
@@ -45,17 +36,13 @@ function allContinents(list) {
   // with every and some
   return allZones.every(x => list.some(y => x === y.continent));
 
-
-
   // with map and a checker function (every, includes)
   const devZones = list.map(dev => {
     return dev.continent;
   })
 
   const checker = (arr, target) => target.every(v => arr.includes(v));
-  return checker(devZones, allZones);
-  
-
+  return checker(devZones, allZones); 
 }
 
 
