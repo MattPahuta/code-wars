fb159b7751561115ee40af332ff2c3069a81a5dc
Matt Pahuta <mhpahuta@gmail.com>
2023-04-16 14:40:24 -0700

    April 16 2023 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,21 +4,7 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
-// *** Arrow function style - 8 kyu
-/*
-Arrow style Functions
-Come here to practice the Arrow style functions Not much else to say good luck!
-Details
-You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
 
-Examples
-These are example of how to convert a number to an ascii Character:
-Javascript => String.fromCharCode(97) // a
-C# => Tools.FromCharCode(97) // a
-*/
-var ArrowFunc = function(arr) {
-  return arr.map(el => String.fromCharCode(el)).join(''); //Complete this function
-}
 
 
 // *** Array Array Array - 7th kyu
