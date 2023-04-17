cf56d5ce97f480ad475e7df53ba7c5bc5ff7f7a9
Matt Pahuta <mhpahuta@gmail.com>
2023-04-17 14:46:23 -0700

    April 17 2023 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -5,6 +5,22 @@
 // ************************************************* //
 
 
+// *** Between Extremes - 7th kyu 
+/*
+Given an array of numbers, return the difference between the largest and smallest values.
+
+For example:
+
+[23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+[1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+*/
+function betweenExtremes(numbers) {
+  // with Math.max/min and spread
+  return Math.max(...numbers) - Math.min(...numbers);
+}
 
 
 // *** Array Array Array - 7th kyu
