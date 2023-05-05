ef4c2d09dfafd3de112cadc3d08de87f8f0cd3d8
Matt Pahuta <mhpahuta@gmail.com>
2023-05-05 10:30:14 -0700

    May 5 2023 challenges

---
+++ 7th-kyu/findTheDivisors.js
@@ -0,0 +1,18 @@
+// *** Find the divisors! - 7 kyu
+/*
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+*/
+function divisors(int) {
+  const totalDivs = [];
+
+  // with for loop and ternary return
+  for (let i = 2; i < int; i += 1) {
+    if (int % i === 0) totalDivs.push(i)
+  }
+  return totalDivs.length ? totalDivs : `${int} is prime`;
+}
