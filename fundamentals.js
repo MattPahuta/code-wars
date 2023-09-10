800236e6c2b4aeea5967ae2e77f54bcd1b14ef6b
Matt Pahuta <mhpahuta@gmail.com>
2023-09-10 13:47:20 -0700

    Sept 10 2023 challenges

--- fundamentals.js
+++ fundamentals.js
@@ -4,6 +4,23 @@
 // *** All challenges completed using JavaScript *** //
 // ************************************************* //
 
+
+// *** Sum of array singles - 7 kyu
+/*
+In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+*/
+function sumArraySingles(arr) {
+  // get unique elements from array
+  // return sum of unique nums
+
+  // with filter, indexOf, lastIndexOf, and reduce
+  return arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num))
+    .reduce((a,b) => a + b, 0);
+}
+
+
 // *** Array combinations - 6 kyu
 /*
 In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
@@ -30,9 +47,7 @@ function uniqueArrays(arr) {
 }
 
 
-
-
-
+// *** Codecademy Challenge *** //
 function encrypt(plainText, shiftNum) {
 
   const plainTextArray = plainText.toUpperCase().split('');
