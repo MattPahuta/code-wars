e3e415f41b4487475d1ff3dd89eb312a20070805
Matt Pahuta <mhpahuta@gmail.com>
2023-11-05 15:18:58 -0800

    Nov 5 2023 challenges

---
+++ 8th-kyu/binaryToDecimal.js
@@ -0,0 +1,7 @@
+// *** Bin to Decimal - 7 kyu
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
